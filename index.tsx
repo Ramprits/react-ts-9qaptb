9deb2a94bccb5a4b5758669e68c348cf44626dff
Ramprits <rampritsahani@outlook.in>
2021-07-04 18:55:22 +0530

    1 file modified

--- index.tsx
+++ index.tsx
@@ -5,12 +5,37 @@ import {
   InMemoryCache,
   ApolloProvider,
   useQuery,
+  createHttpLink,
   gql
 } from '@apollo/client';
 
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri:
+    process.env.REACT_APP_API_URL ||
+    'https://dhanai-fruits.hasura.app/v1/graphql'
+});
+
+const authLink = setContext(({ operationName }, prevCtx) => {
+  const publicOperations = ['login', 'register'];
+  if (
+    operationName &&
+    !publicOperations.includes(operationName.toLowerCase())
+  ) {
+    const token = localStorage.getItem('user_token');
+    return {
+      headers: {
+        ...prevCtx.headers,
+        Authorization: `Bearer ${token}`
+      }
+    };
+  }
+});
+
 const client = new ApolloClient({
-  uri: 'https://dhanai-fruits.hasura.app/v1/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink)
 });
 
 import App from './app';
