6f7ef5f51e218d3904efd4b5c63cccaffbc21937
Ramprits <rampritsahani@outlook.in>
2021-07-04 18:45:32 +0530

    2 files modified

--- index.tsx
+++ index.tsx
@@ -1,5 +1,23 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://dhanai-fruits.hasura.app/v1/graphql',
+  cache: new InMemoryCache()
+});
+
 import App from './app';
 
-render(<App />, document.getElementById('root'));
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
